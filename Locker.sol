cfd356b83400e1bb8617735e633b4787b1430d13
hanhvn <hanhvn2@bap.jp>
2021-05-19 18:21:41 +0700

    Update locker, remove enableTransfer() and add some comments

--- Locker.sol
+++ Locker.sol
@@ -9,18 +9,20 @@ import "./utils/Ownable.sol";
 
 
 contract Locker is Ownable{
-
+    // contains addresses that were in the seeding or private sale,
+    // these addresses will be locked from sending their token to other addresses
     mapping(address => bool) public seedingRoundWhitelist;
     mapping(address => bool) public privateRoundWhitelist;
 
+    // block number indicate the start and ending of lock period for seeding round
     uint256 public seedingStart;
     uint256 public seedingEnd;
-
+    // block number indicate the start and ending of lock period for private sale round
     uint256 public privateSaleStart;
     uint256 public privateSaleEnd;
 
     enum RoundType {SEEDING, PRIVATE}
-    
+
     constructor(uint256 seedingStart_, 
                 uint256 seedingEnd_, 
                 uint256 privateSaleStart_, 
@@ -32,17 +34,26 @@ contract Locker is Ownable{
         privateSaleEnd = privateSaleEnd_;
     }
     
+    /**
+     * @dev Sets the values for {seedingStart} and {seedingEnd},
+     */
     function updateSeedingTime(uint256 newStart, uint256 newEnd) external onlyOwner {
         seedingStart = newStart;
         seedingEnd = newEnd;
     }
-    
+
+    /**
+     * @dev Sets the values for {privateSaleStart} and {privateSaleEnd},
+     */
     function updatePrivateSaleTime(uint256 newStart, uint256 newEnd) external onlyOwner {
         privateSaleStart = newStart;
         privateSaleEnd = newEnd;
     }
 
-
+    /**
+     * @dev Sets the values for {seedingRoundWhitelist} or {privateRoundWhitelist},
+     * the list is depends on value of {round}, valid values: [0: SeedingRound, 1: PrivateSaleRound] 
+     */
     function addWhitelist(address[] memory addresses, RoundType round) external onlyOwner {
         if (round == RoundType.SEEDING) {
             for (uint i = 0; i < addresses.length; i++) {
@@ -56,14 +67,10 @@ contract Locker is Ownable{
             }
         }
     }
-    
-    function enableTransfer(address[] memory addresses) external onlyOwner {
-        for (uint i = 0; i < addresses.length; i++) {
-                seedingRoundWhitelist[addresses[i]] = false;
-                privateRoundWhitelist[addresses[i]] = false;
-            }
-    }
 
+     /**
+     * @dev check the value of {source} revert transaction if this address is in one of the two whitelist and still in lock period
+     */
     function checkLock(address source) external {
         if (seedingRoundWhitelist[source]) {
             if(block.number >= seedingStart && block.number <= seedingEnd) {
