3657adf703ad1a86f83a36ba8b045e7be331945a
hanhvn <hanhvn2@bap.jp>
2021-05-20 18:35:04 +0700

    Improve lock mechanism

--- contracts/Locker.sol
+++ contracts/Locker.sol
@@ -14,40 +14,34 @@ contract Locker is Ownable{
     mapping(address => bool) public seedingRoundWhitelist;
     mapping(address => bool) public privateRoundWhitelist;
 
-    // block number indicate the start and ending of lock period for seeding round
-    uint256 public seedingStart;
-    uint256 public seedingEnd;
-    // block number indicate the start and ending of lock period for private sale round
-    uint256 public privateSaleStart;
+    // block number indicates the ending of lock period for seeding round
+    uint256 public seedingRoundEnd;
+    // block number indicates the ending of lock period for private sale round
     uint256 public privateSaleEnd;
+
+    uint256 minLockPeriod = 1 hours;
 
     enum RoundType {SEEDING, PRIVATE}
 
-    constructor(uint256 seedingStart_, 
-                uint256 seedingEnd_, 
-                uint256 privateSaleStart_, 
-                uint256 privateSaleEnd_) {
-                    
-        seedingStart = seedingStart_;
-        seedingEnd = seedingEnd_;
-        privateSaleStart = privateSaleStart_;
-        privateSaleEnd = privateSaleEnd_;
+    constructor(uint256 seedingRoundLockPeriod, 
+                uint256 privateSaleLockPeriod) {
+        require(seedingRoundLockPeriod >= minLockPeriod, "Invalid lock period for seeding round");
+        require(privateSaleLockPeriod >= minLockPeriod, "Invalid lock period for private sale round");
+
+        seedingRoundEnd = block.number + seedingRoundLockPeriod;
+        privateSaleEnd = block.number + privateSaleLockPeriod;
     }
     
     /**
-     * @dev Sets the values for {seedingStart} and {seedingEnd},
+     * @dev Sets the values for {newLockPeriod},
      */
-    function updateSeedingTime(uint256 newStart, uint256 newEnd) external onlyOwner {
-        seedingStart = newStart;
-        seedingEnd = newEnd;
-    }
+    function updateLockTime(uint256 newLockPeriod, RoundType round) external onlyOwner {
+        require(round == RoundType.SEEDING || round == RoundType.PRIVATE, "Invalid round type");
 
-    /**
-     * @dev Sets the values for {privateSaleStart} and {privateSaleEnd},
-     */
-    function updatePrivateSaleTime(uint256 newStart, uint256 newEnd) external onlyOwner {
-        privateSaleStart = newStart;
-        privateSaleEnd = newEnd;
+        if (round == RoundType.SEEDING)
+            seedingRoundEnd = block.number + newLockPeriod;
+        if (round == RoundType.PRIVATE)
+            privateSaleEnd = block.number + newLockPeriod;
     }
 
     /**
@@ -55,13 +49,15 @@ contract Locker is Ownable{
      * the list type is depends on value of {round}, valid values are: [0: SeedingRound, 1: PrivateSaleRound] 
      */
     function addWhitelist(address[] memory addresses, RoundType round) external onlyOwner {
+        require(round == RoundType.SEEDING || round == RoundType.PRIVATE, "Invalid round type");
+
         if (round == RoundType.SEEDING) {
             for (uint i = 0; i < addresses.length; i++) {
                 seedingRoundWhitelist[addresses[i]] = true;
             }
         }
 
-        if (round == RoundType.PRIVATE) {
+        else if (round == RoundType.PRIVATE) {
             for (uint i = 0; i < addresses.length; i++) {
                 privateRoundWhitelist[addresses[i]] = true;
             }
@@ -72,16 +68,9 @@ contract Locker is Ownable{
      * @dev check the value of {source} address, revert transaction if this address is in one of the two whitelist and still in lock period
      */
     function checkLock(address source) external {
-        if (seedingRoundWhitelist[source]) {
-            if(block.number >= seedingStart && block.number <= seedingEnd) {
-                revert("You can not transfer money during seeding time");
-            }
-        }
-
-        if (privateRoundWhitelist[source]) {
-            if(block.number >= privateSaleStart && block.number <= privateSaleEnd) {
-                revert("You can not transfer money during private sale time");
-            }
-        }
+        if (seedingRoundWhitelist[source])
+            require(block.number >= seedingRoundEnd, "You can not transfer money during seeding time");
+        if (privateRoundWhitelist[source])
+            require(block.number >= privateSaleEnd, "You can not transfer money during private sale time");
     }
 }
