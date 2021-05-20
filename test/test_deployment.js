736016aab934f74d9dc3ce9b7ddbd0bb0b968af7
hanhvn <hanhvn2@bap.jp>
2021-05-20 18:35:29 +0700

    add first test

--- test/test_deployment.js
+++ test/test_deployment.js
@@ -5,20 +5,20 @@ const NFTToken = artifacts.require("NFTToken");
 
 contract("SmartCopyRightToken", async accounts => {
 
-    let erc20;
+    let erc20Token;
     let locker;
     let nftToken;
 
     beforeEach(async () => {
-        erc20 = await SmartCopyRightToken.deployed();
+        erc20Token = await SmartCopyRightToken.deployed();
         locker = await Locker.deployed();
         nftToken = await NFTToken.deployed(); 
 
     });
 
     it("Should put 4000000 token in the first account", async() => {
-        const balance = await erc20.balanceOf.call(accounts[0]);
-        console.log(erc20.address);
+        const balance = await erc20Token.balanceOf.call(accounts[0]);
+        console.log(erc20Token.address);
         console.log(BigInt(balance));
 
     });
