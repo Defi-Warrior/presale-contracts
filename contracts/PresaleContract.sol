970a3e90a82c76c2adcafc8ecf769d08024f8f5f
hanhvn <hanhvn2@bap.jp>
2021-05-25 15:55:54 +0700

    implement presale logics

--- contracts/PresaleContract.sol
+++ contracts/PresaleContract.sol
@@ -24,16 +24,27 @@ struct PresaleSetting {
 
 interface ILocker {
   /**
-   * @dev lock the transfer function of {source} for a specific duration
+   * @dev lock the ability to transfer CORI token of {source} for a specific duration
    */
   function lock(address source, uint256 start, uint256 end) external;
+}
+
+interface ISetting {
+    function start() external view returns (uint256);
+    function end() external view returns (uint256);
+    function price() external view returns (uint256);
+    function lockDuration() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    function minPurchase() external view returns (uint256);
 }
 
 contract Presale is Ownable {
 
     uint256 public totalTokenSold;
 
-    mapping(address=>uint256) public balances;
+    mapping(address => uint256) public balances;
+    mapping(address => uint256) public totalSold;
+    mapping(address => bool) public whitelist;
 
     address public USDT_CONTRACT = 0x40d60E0282356D82358B0De9e5F437401a12f0ab;
     address public BUSD_CONTRACT = 0x68b55C4c19Ee274a68080b156b1e10CdAF34E63E;
@@ -42,64 +53,91 @@ contract Presale is Ownable {
 
     enum TokenType {USDT, BUSD, DAI}
 
-    IERC20 public stableCoin;
     IERC20 public CORI_TOKEN;
 
+    ISetting public SEEDING_SETTING;
+    ISetting public FIRST_ROUND_SETTING;
+    // ISetting public SECOND_ROUND_SETTING;
+    // ISetting public THRID_ROUND_SETTING;
+    ISetting public PUBLIC_SALE_SETTING;
 
-    PresaleSetting public SEEDING_SETTING;
-    PresaleSetting public FIRST_ROUND_SETTING;
-    PresaleSetting public SECOND_ROUND_SETTING;
-    PresaleSetting public THRID_ROUND_SETTING;
-
-    PresaleSetting public currentSetting;
-
-    // Smart Copyright payment
+    ISetting public currentSetting;
     ILocker public locker;
 
 
-    constructor(address lockerAddr) {
-        SEEDING_SETTING = PresaleSetting("Seeding", 100*10**18, 9119140, 10119140, 0, 200000000*10**18, 1000, 15);
-        FIRST_ROUND_SETTING = PresaleSetting("Private Sale Round 1", 200*10**18, 0, 0, 0, 200000000*10**18, 666, 12);
-        SECOND_ROUND_SETTING = PresaleSetting("Private Sale Round 2", 300*10**18, 0, 0, 0, 100000000*10**18, 666, 12);
-        THRID_ROUND_SETTING = PresaleSetting("Private Sale Round 3", 400*10**18, 0, 0, 0, 100000000*10**18, 666, 12);
+    constructor(address lockerAddr, address seedingSetting, address roundOneSetting, address publicSaleSetting) {
+        SEEDING_SETTING = ISetting(seedingSetting);
+        FIRST_ROUND_SETTING = ISetting(roundOneSetting);
+        PUBLIC_SALE_SETTING = ISetting(publicSaleSetting);
+        // SECOND_ROUND_SETTING = ISetting();
+        // THRID_ROUND_SETTING = ISetting();
 
         CORI_TOKEN = IERC20(CORI_CONTRACT);
 
         locker = ILocker(lockerAddr);
     }
 
-    function updatePresaleSetting() public {
-        if (block.number >= SEEDING_SETTING.start && block.number <= SEEDING_SETTING.end)
-            currentSetting = SEEDING_SETTING;
-
-        else if (block.number >= FIRST_ROUND_SETTING.start && block.number <= FIRST_ROUND_SETTING.end)
-            currentSetting = FIRST_ROUND_SETTING;
-
-        else if (block.number >= SECOND_ROUND_SETTING.start && block.number <= SECOND_ROUND_SETTING.end)
-            currentSetting = SECOND_ROUND_SETTING;
-
-        else if (block.number >= THRID_ROUND_SETTING.start && block.number <= THRID_ROUND_SETTING.end)
-            currentSetting = THRID_ROUND_SETTING;
+    function addWhitelist(address[] memory addr) external onlyOwner {
+        for(uint i = 0; i < addr.length; i++)
+            whitelist[addr[i]] = true;
     }
 
+    /**
+   * @dev update the current setting and status of presale
+   */
+    function updatePresaleStatus() public {
+        if (block.number >= SEEDING_SETTING.start() && block.number <= SEEDING_SETTING.end()) {
+            currentSetting = SEEDING_SETTING;
+        }
+        else if (block.number >= FIRST_ROUND_SETTING.start() && block.number <= FIRST_ROUND_SETTING.end()) {
+            currentSetting = FIRST_ROUND_SETTING;
+        }
+        else if (block.number >= PUBLIC_SALE_SETTING.start() && block.number <= PUBLIC_SALE_SETTING.end()) {
+            currentSetting = PUBLIC_SALE_SETTING;
+        }
+        // else if (block.number >= SECOND_ROUND_SETTING.start() && block.number <= SECOND_ROUND_SETTING.end()) {
+        //     currentSetting = SECOND_ROUND_SETTING;
+        //     presaleStatus = PresaleStatus.ON_GOING;
+        // }    
+        // else if (block.number >= THRID_ROUND_SETTING.start() && block.number <= THRID_ROUND_SETTING.end()) {
+        //     currentSetting = THRID_ROUND_SETTING;
+        //     presaleStatus = PresaleStatus.ON_GOING;
+        // }
+    }
+
+    /**
+   * @dev
+   */
     function deposit(address spender, uint256 amount, address tokenAddr) internal {
-        uint256 sellAmount = amount * currentSetting.PRICE;
-        stableCoin = IERC20(tokenAddr);
-
-        stableCoin.transferFrom(spender, address(this), amount);
-
-        locker.lock(spender, currentSetting.start, currentSetting.end + currentSetting.lockDuration);
-
-        CORI_TOKEN.transfer(spender, sellAmount);
-
+        // amount of CORI token that we are going to sell
+        uint256 sellAmount = amount * currentSetting.price();
+        // transfer buyer's stable coin to this contract
+        IERC20(tokenAddr).transferFrom(spender, address(this), amount);
+        // lock CORI token before transfer
+        locker.lock(spender, currentSetting.start(), currentSetting.end() + currentSetting.lockDuration());
+        // transfer CORI token to buyer
+        CORI_TOKEN.transferFrom(owner(), spender, sellAmount);
+        // update user balance and total token sold
         balances[_msgSender()] += sellAmount;
-        
+        totalSold[address(currentSetting)] += sellAmount;
         totalTokenSold += sellAmount;
     }
-
+    
+    /**
+   * @dev user will call this function to buy our token by their stable coins
+   */
     function buyToken(uint256 amount, TokenType tokenType) public {
-        require(amount > 0, "Invest amount must larger than zero");
+        if (address(currentSetting) != address(PUBLIC_SALE_SETTING)) {
+            require(whitelist[_msgSender()], "Not whitelisted address, you are not allowed to purchase in this time");
+        }
+        require(block.number >= currentSetting.start() && block.number < currentSetting.end(), "Presale is not started or has ended");
         require(tokenType == TokenType.USDT || tokenType == TokenType.BUSD || tokenType == TokenType.DAI, "Invalid token type");
+
+        uint256 buyAmount = amount * currentSetting.price();
+
+        require(buyAmount >= currentSetting.minPurchase(), "Invest amount must larger or equal to than mininimum purchase amount");
+        require(totalSold[address(currentSetting)] + buyAmount <= currentSetting.totalSupply(), "No more token to sell in this round");
+
         // require(_seedingAllowances[_msgSender()] >= amount, "Invalid address or the invest amount is higher than allowed");
         address tokenAddr;
         if (tokenType == TokenType.USDT)
@@ -115,6 +153,7 @@ contract Presale is Ownable {
     }
 
     function ownerWithdraw() external onlyOwner {
+        require(block.number > PUBLIC_SALE_SETTING.end(), "Presale is not ended");
         IERC20 usdt = IERC20(USDT_CONTRACT);
         IERC20 busd = IERC20(BUSD_CONTRACT);
         IERC20 dai = IERC20(DAI_CONTRACT);
---
+++ contracts/PresaleSetting.sol
@@ -0,0 +1,90 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-05-09
+*/
+
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.0;
+
+import {Ownable} from "./utils/Ownable.sol";
+
+
+contract PresaleSetting is Ownable {
+    string public name;
+    uint256 public minPurchase;
+    uint256 public start;
+    uint256 public end;
+    uint256 public lockDuration;
+    uint256 public totalSupply;
+    uint256 public price;
+    uint256 public vestingMonth;
+
+    constructor(string memory name_, 
+                uint256 minPurchase_, 
+                uint256 start_, 
+                uint256 end_, 
+                uint256 lockDuration_, 
+                uint256 totalSupply_, 
+                uint256 price_, 
+                uint256 vestingMonth_) {
+        name = name_;
+        minPurchase = minPurchase_;
+        start = start_;
+        end = end_;
+        lockDuration = lockDuration_;
+        totalSupply = totalSupply_;
+        price = price_;
+        vestingMonth = vestingMonth_;
+    }
+
+    function editSetting(string memory name_, 
+                uint256 minPurchase_, 
+                uint256 start_, 
+                uint256 end_, 
+                uint256 lockDuration_, 
+                uint256 totalSupply_, 
+                uint256 price_, 
+                uint256 vestingMonth_) external onlyOwner {
+        name = name_;
+        minPurchase = minPurchase_;
+        start = start_;
+        end = end_;
+        lockDuration = lockDuration_;
+        totalSupply = totalSupply_;
+        price = price_;
+        vestingMonth = vestingMonth_;
+    }
+
+    function setName(string memory newName) external onlyOwner {
+        name = newName;
+    }
+
+    function setMinPurchase(uint256 newValue) external onlyOwner {
+        minPurchase = newValue;
+    }
+
+    function setStart(uint256 newValue) external onlyOwner {
+        start = newValue;
+    }
+
+    function setEnd(uint256 newValue) external onlyOwner {
+        end = newValue;
+    }
+
+    function setLockDuration(uint256 newValue) external onlyOwner {
+        lockDuration = newValue;
+    }
+
+    function setTotalSupply(uint256 newValue) external onlyOwner {
+        totalSupply = newValue;
+    }
+    function setPrice(uint256 newValue) external onlyOwner {
+        price = newValue;
+    }
+
+    function setVestingMonth(uint256 newValue) external onlyOwner {
+        vestingMonth = newValue;
+    }
+
+}
