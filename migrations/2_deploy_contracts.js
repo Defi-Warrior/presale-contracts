6398b3e0b9d2ec72a2b89f912c1bd86d2b2ea6f5
hanhvn <hanhvn2@bap.jp>
2021-05-28 15:53:22 +0700

    Use truffle migrate module to deploy Presale contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,14 +2,56 @@ var SmartCopyrightToken = artifacts.require("SmartCopyRightToken");
 var Locker = artifacts.require("Locker");
 var Setting = artifacts.require("PresaleSetting");
 var Presale = artifacts.require("Presale");
+var StableCoin = artifacts.require("StableCoin");
 
 
-module.exports = function(deployer) {
-  // deployer.deploy(SmartCopyrightToken);
-  // deployer.deploy(Locker);
-  // deployer.deploy(Setting, "Seeding", 1000000000000000000n, 0, 1000, 1, 200000000000000000000000n, 100, 12);
-  // deployer.deploy(Presale, "0xbBF25ffd774162a94f9A1fc01068fA3479BB75f1", 
-  //   "0xbBF25ffd774162a94f9A1fc01068fA3479BB75f1", 
-  //   "0xbBF25ffd774162a94f9A1fc01068fA3479BB75f1", 
-  //   "0xbBF25ffd774162a94f9A1fc01068fA3479BB75f1");
+var locker, seedingSetting, privateSetting, publicSetting, cori, usdt, busd, presale; 
+// testnet addresses
+const usdt_address = "0x5Fe101be7958Def91392650770765Eeb7EC04EDC";
+const busd_address = "0xD84e643EcA06E942044211B50386bbb796E92C8C";
+
+module.exports = async function(deployer) {
+  let block = await web3.eth.getBlock("latest");
+  console.log("block number: ", block.number);
+
+  await deployer.deploy(Locker);
+  locker = await Locker.deployed();
+  
+  await deployer.deploy(Setting, "Seeding", block.number + 1, block.number + 1000, 100, 1, 2000000, 6, 12);
+  seedingSetting = await Setting.deployed();
+
+  await deployer.deploy(Setting, "Private", block.number + 1001, block.number + 2000, 50, 1, 2000000, 6, 12);
+  privateSetting = await Setting.deployed();
+
+  await deployer.deploy(Setting, "Public Sale", block.number + 2001, block.number + 3000, 25, 1, 2000000, 0, 0);
+  publicSetting = await Setting.deployed();
+
+  await deployer.deploy(SmartCopyrightToken);
+  cori = await SmartCopyrightToken.deployed();
+
+  await deployer.deploy(Presale, 
+                        locker.address, 
+                        seedingSetting.address, 
+                        privateSetting.address, 
+                        publicSetting.address, 
+                        usdt_address, 
+                        busd_address, 
+                        cori.address);
+
+  presale = await Presale.deployed();
+
+  await presale.updatePresaleStatus();
+
+  await presale.addWhitelist(["0x60E0F7cDAb17b9B8421c12292Ce8CaC892f5ebee", 
+                              "0x0da95fF7edd5A9A52b790bfA9FD2B638fA3F8eBd", 
+                              "0x7a42581cC461E7FC1069C72D02fe3046af3BFcA0"]);
+
+  await cori.approve(presale.address, BigInt(await cori.totalSupply()));
+
+  await cori.setLocker(locker.address);
+
+  await locker.setPresaleAddress(presale.address);
+
+  console.log("presale address: ", await locker.presaleAddress());
+  console.log("env: ", process.env.PRIVATE_KEY);
 };
