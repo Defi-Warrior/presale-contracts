1782704dc8f28e07e193703b89685ca5c7169711
hanhvn <vonhathanh@gmail.com>
2021-06-17 20:14:56 +0700

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -61,12 +61,21 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     bsctest: {
-    provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, `https://data-seed-prebsc-1-s1.binance.org:8545/`),
+    provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, 'https://data-seed-prebsc-1-s1.binance.org:8545/'),
       network_id: 97,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 1,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      confirmations: 0,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 50,  // # of blocks before a deployment times out  (minimum/default: 50)
       gasPrice: 10000000000,
+      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
+    },
+    mainnet: {
+    provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, 'https://bsc-dataseed1.ninicoin.io'),
+      network_id: 56,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 0,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 50,  // # of blocks before a deployment times out  (minimum/default: 50)
+      gasPrice: 5000000000,
       skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
     },
     // Useful for private networks
