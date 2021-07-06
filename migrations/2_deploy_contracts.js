031e078c6157563edfc7856ca78e064ba2fd0723
hanhvn <vonhathanh@gmail.com>
2021-07-07 06:47:35 +0700

    Update mainnet address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -13,12 +13,12 @@ const StableCoin = artifacts.require("StableCoin");
 
 // var locker, seedingSetting, privateSetting, publicSetting, cori, presale; 
 // testnet addresses
-const usdt_address = "0xF9148233A42787147Cab2690B90ea962Adc22126";
-const busd_address = "0x26C84EAeC7735a3B263bde1368f586791DBB978A";
+// const usdt_address = "0xF9148233A42787147Cab2690B90ea962Adc22126";
+// const busd_address = "0x26C84EAeC7735a3B263bde1368f586791DBB978A";
 
 // mainnet address
-//const usdt_address = "0x55d398326f99059fF775485246999027B3197955";
-//const busd_address = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+const usdt_address = "0x55d398326f99059fF775485246999027B3197955";
+const busd_address = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
 
 module.exports = async function(deployer) {
     let block = await web3.eth.getBlock("latest");
@@ -36,23 +36,23 @@ module.exports = async function(deployer) {
     await deployer.deploy(Setting, "Seeding", block.number + 1, block.number + 86400, 5000, expandTo18Decimals(10000), expandTo18Decimals(500000000), 1, 15);
     seedingSetting = await Setting.deployed();
 
-    await deployer.deploy(Setting, "Private Sale Phase 1", block.number + 1, block.number + 86400, 1000, expandTo18Decimals(100), expandTo18Decimals(100000000), 2, 12);
+    await deployer.deploy(Setting, "Private Sale Phase 1", block.number + 1, 0, 1000, expandTo18Decimals(100), expandTo18Decimals(100000000), 2, 12);
     privateSetting_1 = await Setting.deployed();
 
-    await deployer.deploy(Setting, "Private Sale Phase 2", block.number + 1, block.number + 86400, 909, expandTo18Decimals(100), expandTo18Decimals(150000000), 2, 12);
+    await deployer.deploy(Setting, "Private Sale Phase 2", block.number + 1, 0, 909, expandTo18Decimals(100), expandTo18Decimals(150000000), 2, 12);
     privateSetting_2 = await Setting.deployed();
 
-    await deployer.deploy(Setting, "Private Sale Phase 3", block.number + 1, block.number + 86400, 833, expandTo18Decimals(100), expandTo18Decimals(200000000), 2, 12);
+    await deployer.deploy(Setting, "Private Sale Phase 3", block.number + 1, 0, 833, expandTo18Decimals(100), expandTo18Decimals(200000000), 2, 12);
     privateSetting_3 = await Setting.deployed();
 
-    await deployer.deploy(Setting, "Private Sale Phase 4", block.number + 1, block.number + 86400, 769, expandTo18Decimals(100), expandTo18Decimals(250000000), 2, 12);
+    await deployer.deploy(Setting, "Private Sale Phase 4", block.number + 1, 0, 769, expandTo18Decimals(100), expandTo18Decimals(250000000), 2, 12);
     privateSetting_4 = await Setting.deployed();
 
-    await deployer.deploy(Setting, "Private Sale Phase 5", block.number + 1, block.number + 86400, 714, expandTo18Decimals(100), expandTo18Decimals(300000000), 2, 12);
+    await deployer.deploy(Setting, "Private Sale Phase 5", block.number + 1, 0, 714, expandTo18Decimals(100), expandTo18Decimals(300000000), 2, 12);
     privateSetting_5 = await Setting.deployed();
 
-    await deployer.deploy(DefiWarriorToken);
-    presaleToken = await DefiWarriorToken.deployed();
+    // await deployer.deploy(DefiWarriorToken);
+    // presaleToken = await DefiWarriorToken.deployed();
 
     await deployer.deploy(Presale,
                           locker.address,
@@ -64,13 +64,13 @@ module.exports = async function(deployer) {
                           privateSetting_5.address],
                           usdt_address,
                           busd_address,
-                          presaleToken.address);
+                          "0xFf436Ab14F1cb81f6A02F95378b36DFAB11B6E9f");
 
     presale = await Presale.deployed();
 
-    await presaleToken.approve(presale.address, BigInt(await presaleToken.totalSupply()));
+    // await presaleToken.approve(presale.address, BigInt(await presaleToken.totalSupply()));
 
-    await presaleToken.setLocker(locker.address);
+    // await presaleToken.setLocker(locker.address);
 
     await locker.setPresaleAddress(presale.address);
 };
