8af0f03c732fcac00f9c1e972c1dd7d5347c27bb
hanhvn <vonhathanh@gmail.com>
2021-08-12 14:44:21 +0700

    update whitelist

--- utils_scripts/add_whitelist.py
+++ utils_scripts/add_whitelist.py
@@ -10,13 +10,13 @@ from web3 import Web3
 # PROVIDER = os.environ.get("PROVIDER", "https://data-seed-prebsc-1-s1.binance.org:8545/")
 # mainnet provider
 PROVIDER = "https://bsc-dataseed.binance.org/"
-AIRDROP_ADDRESS = os.environ.get("AIR_DROP_ADDRESS", '0x1941B6189aa855732898a16bD44694d351F725a5')
+AIRDROP_ADDRESS = os.environ.get("AIR_DROP_ADDRESS", '0x9A39059b8FF6efEAC97Df9a189C4106E539c48f1')
 with open("./air_drop_abi.json", "r") as f:
     ABI = json.load(f)
 
 
 def main(args: dict):
-    addresses = pd.read_csv(args["file"])["Wallet"].tolist()
+    addresses = pd.read_csv(args["file"])["Your MetaMask Wallet address "].tolist()
 
     web3 = Web3(Web3.HTTPProvider(PROVIDER))
 
@@ -30,6 +30,7 @@ def main(args: dict):
 
     input_addr = []
     for i, addr in enumerate(addresses):
+        addr = addr.strip()
         if Web3.isAddress(addr):
             if Web3.isChecksumAddress(addr):
                 input_addr.append(addr)
@@ -40,8 +41,9 @@ def main(args: dict):
             print("Invalid address: ", addr)
             continue
         if len(input_addr) == 100 or i == len(addresses) - 1:
-            amounts = [666000000000000000000] * len(input_addr)
-            tx = contract.functions.updateBalance(input_addr, amounts).buildTransaction({
+            print(input_addr)
+            permission = [True] * len(input_addr)
+            tx = contract.functions.updateWhitelist(input_addr, permission).buildTransaction({
                 'chainId': os.environ.get("CHAIN_ID", 56),
                 'gasPrice': Web3.toWei('5', 'gwei'),
                 'nonce': nonce,
