13e4f391d54a292e38085e0eed83316938546bf7
hanhvn <vonhathanh@gmail.com>
2021-08-16 15:32:51 +0700

    move old contract to old_contracts folder

--- test/test_lock_v2.js
+++ test/test_lock_v2.js
@@ -22,7 +22,10 @@ contract("DefiWarriorToken", async accounts => {
 
         presaleToken = await DefiWarriorToken.new();
 
-        await presaleToken.transfer(accounts[2], expandTo18Decimals(9999999900))
+        await presaleToken.transfer(accounts[5], expandTo18Decimals(10000000000))
+        await presaleToken.transfer(accounts[0], 10000, {from: accounts[5]})
+        await presaleToken.transfer(accounts[1], 10000, {from: accounts[5]})
+        await presaleToken.transfer(accounts[2], 10000, {from: accounts[5]})
 
         await presaleToken.setLocker(locker.address);
     });
@@ -30,24 +33,44 @@ contract("DefiWarriorToken", async accounts => {
     it("Successfully lock all token", async() => {
         let block = await web3.eth.getBlock("latest");
 
-        await locker.lock(accounts[0], expandTo18Decimals(100), block.number, block.number + 10000);
+        await locker.lock(accounts[0], 10000, block.number, block.number + 100);
 
         let lockRecord = await locker.lockRecords(accounts[0]);
 
-        await expectThrow(presaleToken.transfer(accounts[2], expandTo18Decimals(1)));
+        await expectThrow(presaleToken.transfer(accounts[2], 201));
 
         block = await web3.eth.getBlock("latest");
 
-        await presaleToken.transfer(accounts[2], BigNumber((block.number - lockRecord.start)*10**16))
+        await presaleToken.transfer(accounts[2], BigNumber((block.number - lockRecord.start)*100))
     });
 
     it("Successfully transfer 5% after IDO open", async() => {
         let block = await web3.eth.getBlock("latest");
 
-        await locker.lock(accounts[0], expandTo18Decimals(100), block.number, block.number + 10000);
+        await locker.lock(accounts[0], 10000, block.number + 1, block.number + 101);
         await locker.unlockForIDO(true);
 
-        assert.equal((await locker.getLockedAmount(accounts[0])).toString(), "94980000000000000000")
+        assert.equal(await locker.getLockedAmount(accounts[0]), 9400)
+    });
+
+    it("Successfully transfer token by other user", async() => {
+        let block = await web3.eth.getBlock("latest");
+        await locker.lock(accounts[0], 10000, block.number + 1, block.number + 101);
+        await presaleToken.transfer(accounts[1], 100, {from: accounts[2]})
+        assert.equal(await presaleToken.balanceOf(accounts[2]), 9900);
+    });
+
+    it("Cant lock token by other accounts", async() => {
+        let block = await web3.eth.getBlock("latest");
+        await expectThrow(locker.lock(accounts[0], 10000, block.number + 1, block.number + 101, {from: accounts[1]}));
+    });
+
+    it("Update lock amount after relock", async() => {
+        let block = await web3.eth.getBlock("latest");
+        await locker.lock(accounts[0], 10000, block.number + 1, block.number + 101);
+        assert.equal(await locker.getLockedAmount(accounts[0]), 10000)
+        await locker.lock(accounts[0], 20000, block.number + 2, block.number + 102);
+        assert.equal(await locker.getLockedAmount(accounts[0]), 20000)
     });
 
 })
